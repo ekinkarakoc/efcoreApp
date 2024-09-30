725556d98fb1f726ac12d1bfb575543d8c5f7c90
Ekin KARAKOÇ <ekinkarakoc1@gmail.com>
2024-09-30 17:50:44 +0300

    first commit

---
+++ Data/Kurs.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace efcoreApp.Data
+{
+    public class Kurs
+    {
+        [Key]
+        public int KursId { get; set; }
+        public string? Baslik { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace efcoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+
+    }
+}
